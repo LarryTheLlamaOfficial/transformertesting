d3db4f74b16b7e13e1795ba43a704b2021f9556d
Matthew Hackett <123338547+LarryTheLlamaOfficial@users.noreply.github.com>
2024-12-29 15:21:17 +1100

    Starting work

---
+++ main.py
@@ -0,0 +1,14 @@
+
+import torch
+import torch.nn as nn
+
+class Transformer(nn.Module):
+    def __init__(
+            self,
+            num_tokens,
+            dim_model,
+            num_heads,
+            num_encoder_layers,
+            num_decoder_layers,
+            dropout_p
+    )
